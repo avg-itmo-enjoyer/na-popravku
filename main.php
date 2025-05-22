25cff10bf867f49a5e3b48c1d59e43ae81048c45
Kharlamov Vladislav <kharlamov.workspace@gmail.com>
2025-05-22 23:29:35 +0300

    "Small refactoring"

--- main.php
+++ main.php
@@ -1,30 +1,48 @@
 <?php
 
+/**
+ * @param string $string
+ * @return iterable<string>
+ */
+function split_str_to_tokens(string $string): iterable {
+    $tokens = array();
+    $word_buff = "";
+
+    for ($i = 0; $i < grapheme_strlen($string); ++$i) {
+        if (IntlChar::isWhitespace($string[$i])) {
+            $tokens[] = $word_buff;
+            $word_buff = "";
+        } else {
+            $word_buff .= $string[$i];
+        }
+    }
+    if (!empty($word_buff))
+        $tokens[] = $word_buff;
+
+    return $tokens;
+}
+
+/**
+* @param iterable<string> $tokens
+* @return iterable<string, int>
+*/
+function token_count(iterable $tokens): iterable {
+    $map = array();
+    foreach ($tokens as $token) {
+        if (array_key_exists($token, $map)) {
+            $map[$token]++;
+        } else {
+            $map[$token] = 1;
+        }
+    }
+    return $map;
+}
+
 //TODO: proper way to read input string from different sources (stdin / file / cli arg)
 $string = trim(fgets(STDIN));
 
-$tokens = array();
-$word_buff = "";
-//TODO: parse tokens
-for ($i = 0; $i < grapheme_strlen($string); ++$i) {
-    if (IntlChar::isWhitespace($string[$i])) {
-        $tokens[] = $word_buff;
-        $word_buff = "";
-    } else {
-        $word_buff .= $string[$i];
-    }
-}
-if (!empty($word_buff))
-    $tokens[] = $word_buff;
-
-$map = array();
-foreach ($tokens as $token) {
-    if (array_key_exists($token, $map)) {
-        $map[$token]++;
-    } else {
-        $map[$token] = 1;
-    }
-}
+$tokens = split_str_to_tokens($string);
+$map    = token_count($tokens);
 
 echo "Result:\n";
 foreach ($map as $key => $value)
